11d5d178d08a8a130b496fbe6573ab20b36aec37
Michael Myer <m-myer@live.com>
2023-11-19 14:50:05 -0700

    update

--- server.R
+++ server.R
@@ -1,3 +1,9 @@
 server <- function(session, input, output) {
   
+  output$header <- renderUI({
+    tagList(
+      h3(paste("Selected Algorithm: ", input$schedulingChoices))
+    )
+  })
+  
 }
--- ui.R
+++ ui.R
@@ -20,10 +20,65 @@ ui <- fluidPage(
                  options = list(
                    placeholder = 'Please select an option below'),
                ),
+               
+               conditionalPanel(
+                 condition = "input.schedulingChoices == 'Shortest Job First'",
+                 
+                 checkboxInput(
+                   inputId = "preemptive",
+                   label = "Preemptive", 
+                   value = FALSE
+                 )
+               ),
+               
+               conditionalPanel(
+                 condition = "input.schedulingChoices == 'Round Robin'",
+                 
+                 numericInput(
+                   inputId = "timeQuantum",
+                   label = strong("Time Quantum (in Millisesconds)"),
+                   value = 10, 
+                   min = 1,
+                   step = 1
+                 )
+               ), #RR Conditional Panel
+               
+               radioButtons(
+                 inputId = "simType",
+                 label = strong("Simulation Type"),
+                 choices = list("Runtime",
+                                "Throughput"),
+                 selected = "Runtime",
+                 inline = TRUE
+               ),
+               
+               conditionalPanel(
+                 condition = "input.simType == 'Runtime'",
+
+                 numericInput(
+                   inputId = "runtime",
+                   label = strong("Simulation Runtime (in seconds?)"),
+                   value = NULL,
+                   min = 1,
+                   step = 1
+                 )
+               ),
+               
+               conditionalPanel(
+                 condition = "input.simType == 'Throughput'",
+
+                 numericInput(
+                   inputId = "numProcesses",
+                   label = strong("Total Number of Processes"),
+                   value = NULL,
+                   min = 1,
+                   step = 1
+                 )
+               )
              ),
              
              mainPanel(
-               h1("Stuff will go here")
+               uiOutput("header")
              )
            )
   )
