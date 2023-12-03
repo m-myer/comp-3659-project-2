78e7c1385712f493253f0207d18fc3914d0b7e4d
Michael Myer <m-myer@live.com>
2023-12-03 04:53:54 -0700

    added functions, round robin

--- server.R
+++ server.R
@@ -8,12 +8,21 @@ server <- function(session, input, output) {
   
   df_processes <- reactiveVal()
   
-  df_fcfs <- reactiveValues(processes = data.frame(),
+  df_fcfs <- reactiveValues(new = data.frame(),
                             readyQueue = data.frame(),
                             running = data.frame(),
                             waiting = data.frame(),
                             terminated = data.frame(),
                             cpuIdleTime = 0)
+  
+  df_rr <- reactiveValues(new = data.frame(),
+                          readyQueue = data.frame(),
+                          running = data.frame(),
+                          waiting = data.frame(),
+                          terminated = data.frame(),
+                          cpuIdleTime = 0,
+                          quantum = 0,
+                          timeSlice = 0)
   
   
   
@@ -23,17 +32,27 @@ server <- function(session, input, output) {
     req(iv$is_valid()) # validate user inputs
     
     df_processes(SpawnProcesses())
-    df_fcfs$processes <- df_processes()
+    
+    if(input$schedulingChoices == "First Come First Serve"){
+      df_fcfs$new <- df_processes()
+    }
+    
+    if(input$schedulingChoices == "Round Robin"){
+      df_rr$new <- df_processes()
+      df_rr$quantum <- input$timeQuantum
+    }
+    
     clock$count <- 0
     clock$timer <- reactiveTimer(100)
     clock$active <- TRUE
     
   })
   
-  observeEvent(req(input$schedulingChoices == 'First Come First Serve' && clock$timer()), { # every clock tick
+  
+  observeEvent(req(input$schedulingChoices == "First Come First Serve" && clock$timer()), { # every clock tick
     req(clock$active == TRUE) # only if simulation is running
     
-    UpdateSim(df_fcfs)
+    RunFCFS(df_fcfs)
     clock$count <- clock$count + 1
     
     if(nrow(df_fcfs$terminated) == input$numProcesses){
@@ -41,6 +60,17 @@ server <- function(session, input, output) {
     }
   })
   
+  
+  observeEvent(req(input$schedulingChoices == "Round Robin" && clock$timer()), { # every clock tick
+    req(clock$active == TRUE) # only if simulation is running
+    
+    RunRR(df_rr)
+    clock$count <- clock$count + 1
+    
+    if(nrow(df_rr$terminated) == input$numProcesses){
+      clock$timer <- reactiveTimer(Inf)
+    }
+  })
   
   # Outputs -----------------------------------------------------
   
@@ -56,7 +86,7 @@ server <- function(session, input, output) {
   })
   
   output$fcfsNew <- renderTable({
-    df_fcfs$processes
+    df_fcfs$new
   })
   
   output$fcfsReady <- renderTable({
@@ -73,6 +103,30 @@ server <- function(session, input, output) {
   
   output$fcfsTerminated <- renderTable({
     df_fcfs$terminated
+  })
+  
+  output$rrSlice <- renderTable({
+    c(df_rr$timeSlice, df_rr$quantum)
+  })
+  
+  output$rrNew <- renderTable({
+    df_rr$new
+  })
+  
+  output$rrReady <- renderTable({
+    df_rr$readyQueue
+  })
+  
+  output$rrRunning <- renderTable({
+    df_rr$running
+  })
+  
+  output$rrWaiting <- renderTable({
+    df_rr$waiting
+  })
+  
+  output$rrTerminated <- renderTable({
+    df_rr$terminated
   })
   
   # output$processTable <- renderTable({
@@ -122,21 +176,11 @@ server <- function(session, input, output) {
   }
   
   
-  UpdateSim <- function(df){
-    if(nrow(df$processes) > 0 && df$processes[1,"Arrival"] == clock$count){ # arrival time for next process
-      newProcs <- nrow(subset(df$processes, df$processes$Arrival == clock$count)) # collect row # for new processes
-      ChangeState(df, "readyQueue", "processes", c(1:newProcs)) # added row 1 to nrow
-    }
+  RunFCFS <- function(df){
     
-    if(nrow(df$readyQueue) > 0){ # processes in ready state
-      if(nrow(df$running) == 0){ # no process currently running
-        ChangeState(df, "running", "readyQueue", c(1))
-        df$running$Last.CPU.Burst <- 0
-      }
-      
-      df$readyQueue$Total.Wait.Time <- df$readyQueue$Total.Wait.Time + 1
-      df$readyQueue$Turnaround.Time <- df$readyQueue$Turnaround.Time + 1
-    }
+    CheckNewProcesses(df)
+    CheckWaiting(df)
+    CheckReadyQueue(df)
     
     if(nrow(df$running) == 1){ # process currently running
       if(df$running$Number.of.Event.Bursts != 0 && df$running$Next.Event.Time == df$running$CPU.Burst.Time){ #processes has an IO burst
@@ -155,6 +199,73 @@ server <- function(session, input, output) {
     } else{ # wasted cpu time (for cpu utilization)
       df$cpuIdleTime <- df$cpuIdleTime + 1
     }
+  }
+  
+  
+  RunRR <- function(df){
+    
+    CheckNewProcesses(df)
+    CheckWaiting(df)
+    CheckReadyQueue(df)
+    
+    if(nrow(df$running) == 1){ # process currently running
+      if(df$running$Number.of.Event.Bursts != 0 && df$running$Next.Event.Time == df$running$CPU.Burst.Time){ #processes has an IO burst
+        df$running[1,"Number.of.Event.Bursts"] <- df$running[1, "Number.of.Event.Bursts"] - 1
+        ChangeState(df, "waiting", "running", c(1))
+        df$timeSlice <- 0
+        df$waiting <- df$waiting[order(df$waiting$Event.Burst.Time, decreasing = FALSE),]
+      } else{
+        df$running$Last.CPU.Burst <- df$running$Last.CPU.Burst + 1
+        df$running$CPU.Burst.Time <- df$running$CPU.Burst.Time - 1
+        df$running$Turnaround.Time <- df$running$Turnaround.Time + 1
+        df$timeSlice <- df$timeSlice + 1
+        
+        if(df$timeSlice == df$quantum){
+          ChangeState(df, "readyQueue", "running", c(1))
+          df$timeSlice <- 0
+          
+        } else if(df$running[1,"CPU.Burst.Time"] == 0){ # processes finishes running
+          ChangeState(df, "terminated", "running", c(1))
+          df$timeSlice <- 0
+        }
+      }
+    } else{ # wasted cpu time (for cpu utilization)
+      df$cpuIdleTime <- df$cpuIdleTime + 1
+    }
+  }
+  
+  
+  ChangeState <- function(df, df_to, df_from, processes){
+    
+    df[[df_to]] <- rbind(df[[df_to]], df[[df_from]][processes,])
+    df[[df_from]] <- df[[df_from]][-processes,]
+  }
+  
+  
+  CheckNewProcesses <- function(df){
+    
+    if(nrow(df$new) > 0 && df$new[1,"Arrival"] == clock$count){ # arrival time for next process
+      newProcs <- nrow(subset(df$new, df$new$Arrival == clock$count)) # collect row # for new processes
+      ChangeState(df, "readyQueue", "new", c(1:newProcs)) # added row 1 to nrow
+    }
+  }
+  
+  
+  CheckReadyQueue <- function(df){
+    
+    if(nrow(df$readyQueue) > 0){ # processes in ready state
+      if(nrow(df$running) == 0){ # no process currently running
+        ChangeState(df, "running", "readyQueue", c(1))
+        df$running$Last.CPU.Burst <- 0
+      }
+      
+      df$readyQueue$Total.Wait.Time <- df$readyQueue$Total.Wait.Time + 1
+      df$readyQueue$Turnaround.Time <- df$readyQueue$Turnaround.Time + 1
+    }
+  }
+  
+  
+  CheckWaiting <- function(df){
     
     if(nrow(df$waiting) > 0){ # processes in waiting state
       df$waiting$Event.Burst.Time <- df$waiting$Event.Burst.Time - 1
@@ -165,14 +276,6 @@ server <- function(session, input, output) {
         ChangeState(df, "readyQueue", "waiting", c(1:ioComplete))
       }
     }
-    
-  }
-  
-  
-  ChangeState <- function(df, df_to, df_from, processes){
-    
-    df[[df_to]] <- rbind(df[[df_to]], df[[df_from]][processes,])
-    df[[df_from]] <- df[[df_from]][-processes,] 
   }
   
   # Input Validation --------------------------------------------
